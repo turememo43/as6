871af3d07ebea99e23b5ddf361b55be6f0508c78
turememo43 <ture.memo.7@gmail.com>
2024-07-16 22:30:56 +0300

    methods and classes

---
+++ app/src/main/java/com/mehmetture/project11_methods/MainActivity.java
@@ -0,0 +1,94 @@
+package com.mehmetture.project11_methods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String username;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("on create called");
+
+        testMethod();
+
+        System.out.println(math(3,6));
+
+        System.out.println(newMethod("taycan"));
+
+        username = "parzi";
+
+        System.out.println("oncreate username: " + username);
+
+        makeMusicians();
+    }
+
+    public void makeMusicians(){
+
+     Musicians james = new Musicians("james","guitar",47);
+        System.out.println(james.instrument);
+        System.out.println(james.age);
+        System.out.println(james.name);
+
+    }
+
+
+
+
+
+
+    public void testMethod(){
+        int x = 4 + 4 ;
+        System.out.println("value of x: " + x);
+    }
+
+    public int math(int a , int b){
+
+        return a + b;
+    }
+
+    public String newMethod(String string){
+
+        return string + " new Method";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("on resume called");
+
+        username = "alcatraz";
+
+        System.out.println("onresume username: " + username);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        System.out.println("on stop called");
+
+        username = "lazarus";
+
+        System.out.println("onstop username: " + username);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        System.out.println("on pause called");
+
+        username = "kalibre";
+
+        System.out.println("onpause username: " + username);
+    }
+}
---
+++ app/src/main/java/com/mehmetture/project11_methods/Musicians.java
@@ -0,0 +1,26 @@
+package com.mehmetture.project11_methods;
+
+public class Musicians {
+
+
+    // attribute
+    String name;
+    String instrument;
+    int age;
+
+
+
+   // constructer
+
+    public Musicians(String name, String instrument, int age) {
+        this.name = name;
+        this.instrument = instrument;
+        this.age = age;
+        System.out.println("constructer called");
+    }
+
+
+
+
+
+}
